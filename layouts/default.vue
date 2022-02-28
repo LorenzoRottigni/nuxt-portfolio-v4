59b01c65e183809ee81287d12a117678fbb75a53
LorenzoRottigni <lorenzo@rottigni.net>
2022-02-28 22:51:05 +0100

    scaffold

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+#app.full-height.bg-circuit.bg-dark.border.border-primary
+  h1.text-primary layout default
+  Nuxt
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+extends ./../layouts/default.pug
+
+block content
+  h1 vue router page
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
