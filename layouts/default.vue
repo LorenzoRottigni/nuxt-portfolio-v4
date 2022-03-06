ddae395d15517fe307f099788a90d38fb0771bab
LorenzoRottigni <lorenzo@rottigni.net>
2022-03-06 21:58:07 +0100

    components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,21 @@
 <template lang="pug">
-#app.full-height.bg-circuit.bg-dark.border.border-primary
-  h1.text-primary layout default
-  Nuxt
+#app.full-height.bg-circuit.bg-dark.border-primary
+  //position absolute overlay
+  FPSOverlay
+  //vue router pages, pages extend pug layout
+  main.h-100
+    Nuxt
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import FPSOverlay from '~/components/FPSOverlay.vue'
 export default Vue.extend({
-
+    components: { FPSOverlay }
 })
 </script>
+
+<style lang="sass" scoped>
+#app
+  border: ridge 10px
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 extends ./../layouts/default.pug
 
-block content
-  h1 vue router page
+block hero-content
+  p.text-white Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, eveniet reprehenderit nesciunt doloremque id laudantium reiciendis exercitationem minus sit accusantium qui adipisci tenetur illum inventore odio eius officiis architecto deserunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea hic consectetur sint non repellendus, odio sed eaque magnam, ipsa ratione necessitatibus officia corporis similique voluptates ut harum unde id culpa.
+
 </template>
 
 <script lang="ts">
